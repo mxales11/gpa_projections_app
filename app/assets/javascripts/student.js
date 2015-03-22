25362edeab07d1561fcdf84d79a10be02a8365c3
mxales11 <mxales11@smumn.edu>
2015-03-22 01:40:04 +0000

    changing display

--- app/assets/javascripts/student.js
+++ app/assets/javascripts/student.js
@@ -1,13 +1,5 @@
 $(document).ready(function() {
 
-$('#add_taken_course_form').hide();
-
-$('#add_taken_course_button').click(function() {
-   $('#add_taken_course_form').show();
-   $('#add_taken_course_button').hide();
-});
-
-
 
 $(function() {
     function updateCheckbox() {
